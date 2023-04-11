ac506e8a7278523348f76e33fc903d17c01754ff
karlos1000 <karlos.0@live.com.mx>
2023-04-11 13:30:29 -0600

    Carga inicial

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # ejercicio002
+Create a Vue component that displays a counter on a web page.
+The counter must start at zero and must have two buttons, one to increase the counter and one to decrease it.
+You must make use of the Vue directives v-on and v-model to manipulate the state of the counter.
 
 ## Project setup
 ```
---
+++ src/components/Counter.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h1>{{ count }}</h1>
+        <input type="number" v-model="count">
+    </div>
+    <div>
+        <button v-on:click="increment">Aumentar</button>
+        <button v-on:click="decrement">Disminuir</button>
+    </div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+    name: 'Counter',
+    props: {
+    },
+    setup() {
+        let count = ref(0);
+
+        const increment = () => {
+            count.value ++;
+        }
+
+        const decrement = () => {
+            count.value --;
+        }
+
+        return {
+            count,
+            increment,
+            decrement,
+        }
+    }
+}
+</script>
+
+<style scoped></style>
